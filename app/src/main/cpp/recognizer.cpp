abf5650401d6b66dea36e378b6fda34aa7fd716d
kaicheng.wang <kwang1993@qq.com>
2022-07-04 15:49:37 +0800

    Writing GetResult return cases in recognizer.cpp

--- app/src/main/cpp/recognizer.cpp
+++ app/src/main/cpp/recognizer.cpp
@@ -53,8 +53,24 @@ void Recognizer::InitState() {
     state_ = RECOGNIZER_INITIALIZED;
 }
 
-void Recognizer::InitRescoring() {
-    //    todo
+void Recognizer::InitRescoring() { // ?????
+    if (model_->graph_lm_fst_) {
+        fst::CacheOptions cache_opts(true, -1);
+        fst::ArcMapFstOptions mapfst_opts(cache_opts);
+        fst::StdToLatticeMapper<BaseFloat> mapper; // maps a normal arc (StdArc) to a LatticeArc by putting the StdArc weight as the first element of the LatticeWeight. Useful when doing LM rescoring.
+        lm_to_subtract_ = new fst::ArcMapFst<fst::StdArc, LatticeArc, fst::StdToLatticeMapper<BaseFloat> >(
+                *model_->graph_lm_fst_, mapper, mapfst_opts);
+        carpa_to_add_ = new ConstArpaLmDeterministicFst(model_->const_arpa_);
+
+        if (model_->rnnlm_enabled_) {
+            int lm_order = 4;
+            rnnlm_info_ = new kaldi::rnnlm::RnnlmComputeStateInfo(
+                    model_->rnnlm_compute_opts_, model_->rnnlm_, model_->word_embedding_mat_);
+            rnnlm_to_add_ = new kaldi::rnnlm::KaldiRnnlmDeterministicFst(lm_order, *rnnlm_info_);
+            rnnlm_to_add_scale_ = new fst::ScaleDeterministicOnDemandFst(0.5, rnnlm_to_add_);
+            carpa_to_add_scale_ = new fst::ScaleDeterministicOnDemandFst(-0.5, carpa_to_add_);
+        }
+    }
 }
 
 bool Recognizer::AcceptWaveForm(const short *sdata, int len) {
@@ -156,15 +172,142 @@ const char* Recognizer::Result(){
     return GetResult();
 }
 
-const char *Recognizer::StoreEmptyReturn() {
 
-}
 
-const char* Recognizer::GetResult() {
+const char* Recognizer::GetResult() { // ?????
     if (decoder_->NumFramesDecoded() == 0) {
         return StoreEmptyReturn();
     }
-    
+    // Original from decoder, subtracted graph weight, rescored with carpa, rescored with rnnlm
+    CompactLattice clat, slat, tlat, rlat;
+    clat = decoder_->GetLattice(decoder_->NumFramesDecoded(), true); // num_frames_to_include, use_final_probs
+
+    if (lm_to_subtract_ && carpa_to_add_) { // from InitRescoring
+        Lattice lat, composed_lat;
+
+        // Delete old score
+        ConvertLattice(clat, &lat);
+        fst::ScaleLattice(fst::GraphLatticeScale(-1.0), &lat);
+        fst::Compose(lat, *lm_to_subtract, &composed_lat);
+        fst::Invert(&composed_lat);
+        DeterminizeLattice(composed_lat, &slat);
+        fst::ScaleLattice(fst::GraphLatticeScale(-1.0), &slat);
+
+        // Add CARPA score
+        TopSortCompactLatticeIfNeeded(&slat);
+        ComposeCompactLatticeDeterministic(slat, capra_to_add_, &tlat);
+
+        // Rescore with RNNLM score on top if needed
+        if (rnnlm_to_add_scale_) {
+            ComposeLatticePrunedOptions compose_opts;
+            compose_opts.lattice_compose_beam = 3.0;
+            compose_opts.max_arcs = 3000;
+            fst::ComposeDeterministicOnDemandFst<StdArc> combined_rnnlm(carpa_to_add_scale_, rnnlm_to_add_scale_);
+
+            TopSortCompactLatticeIfNeeded(&tlat);
+            ComposeCompactLatticePruned(compose_opts, tlat,
+                                        &combined_rnnlm, &rlat);
+            rnnlm_to_add_->Clear();
+        } else {
+            rlat = tlat;
+        }
+    } else {
+        rlat = clat;
+    }
+
+    // Pruned composition can return empty lattice.
+    if (rlat.Start() != 0) {
+        return StoreEmptyReturn();
+    }
+
+    // Apply rescoring weight
+    fst::ScaleLattice(fst::GraphLatticeScale(0.9), &rlat);
+    if (max_alternatives_ == 0) {
+        return MbrResult(rlat);
+    } else if (nlsml_) {
+        return NlsmlResult(rlat);
+    } else {
+        return NbestResult(rlat);
+    }
+}
+
+const char *Recognizer::NbestResult(CompactLattice &rlat){
+    //todo
+}
+
+const char *Recognizer::MbrResult(CompactLattice &rlat){
+    //todo
+}
+
+const char *Recognizer::NlsmlResult(CompactLattice &rlat){
+    //todo
+}
+
+
+const char *Recognizer::StoreEmptyReturn() {
+    if (! max_alternatves_) {
+        return StoreReturn("{\"text\": \"\"}")
+    } else if (nlsml_) {
+        return StoreReturn("<?xml version=\"1.0\"?>\n"
+                           "<result grammar=\"default\">\n"
+                           "<interpretation confidence=\"1.0\">\n"
+                           "<instance/>\n"
+                           "<input><noinput/></input>\n"
+                           "</interpretation>\n"
+                           "</result>\n");
+    } else {
+        return StoreReturn("{\"alternatives\" : [{\"text\": \"\", \"confidence\" : 1.0}] }");
+    }
+}
+
+
+// Store result in recognizer and return as const string
+const char *Recognizer::StoreReturn(const string &res)
+{
+    last_result_ = res;
+    return last_result_.c_str();
+}
+
+const char* Recognizer::FinalResult()
+{
+    if (state_ != RECOGNIZER_RUNNING) {
+        return StoreEmptyReturn();
+    }
+
+    feature_pipeline_->InputFinished();
+    UpdateSilenceWeights();
+    decoder_->AdvanceDecoding();
+    decoder_->FinalizeDecoding();
+    state_ = RECOGNIZER_FINALIZED;
+    GetResult();
+
+    // Free some memory while we are finalized, next
+    // iteration will reinitialize them anyway
+    delete decoder_;
+    delete feature_pipeline_;
+    delete silence_weighting_;
+    delete spk_feature_;
+
+    feature_pipeline_ = nullptr;
+    silence_weighting_ = nullptr;
+    decoder_ = nullptr;
+    spk_feature_ = nullptr;
+
+    return last_result_.c_str();
+}
+
+const char* Recognizer::PartialResult()
+{
+    //todo
+}
+
+void Recognizer::Reset()
+{
+    if (state_ == RECOGNIZER_RUNNING) {
+        decoder_->FinalizeDecoding();
+    }
+    StoreEmptyReturn();
+    state_ = RECOGNIZER_ENDPOINT;
 }
 
 Recognizer::~Recognizer() {
@@ -173,6 +316,13 @@ Recognizer::~Recognizer() {
     delete silence_weighting_;
     delete decode_fst_;
     delete spk_feature_;
+
+    delete lm_to_subtract_;
+    delete carpa_to_add_;
+    delete carpa_to_add_scale_;
+    delete rnnlm_info_;
+    delete rnnlm_to_add_;
+    delete rnnlm_to_add_scale_;
 
     model_->Unref();
     if (spk_model_) {
--- app/src/main/cpp/recognizer.h
+++ app/src/main/cpp/recognizer.h
@@ -20,6 +20,9 @@ public:
     Recognizer(Model *model);
     bool AcceptWaveform(const short* sdata, int len);
     const char* Result();
+    const char* FinalResult();
+    const char* PartialResult();
+    void Reset();
 private:
     ~Recognizer();
     void InitState();
@@ -27,17 +30,30 @@ private:
     bool AcceptWaveform(Vector<BaseFloat>& wave);
     void CleanUp();
     void UpdateSilenceWeights();
-    const char *StoreEmptyReturn();
     const char* GetResult();
+    const char *MbrResult(CompactLattice &clat);
+    const char *NbestResult(CompactLattice &clat);
+    const char *NlsmlResult(CompactLattice &clat);
+    const char *StoreEmptyReturn();
+    const char *StoreReturn(const string &res);
+
 
     Model* model_ = nullptr;
-    string last_result_;
 
     kaldi::OnlineNnet2FeaturePipeline *feature_pipeline_ = nullptr; // main feature extraction pipeline
     kaldi::OnlineSilenceWeighting *silence_weighting_ = nullptr; // weighting silence in ivector adaptation
     fst::LookaheadFst<fst::StdArc, int32> *decode_fst_ = nullptr;
     kaldi::SingleUtteranceNnet3IncrementalDecoder *decoder_ = nullptr;
     kaldi::OnlineBaseFeature *spk_feature_ = nullptr;
+    // Rescoring
+    fst::ArcMapFst<fst::StdArc, LatticeArc, fst::StdToLatticeMapper<BaseFloat> > *lm_to_subtract_ = nullptr;
+    kaldi::ConstArpaLmDeterministicFst *carpa_to_add_ = nullptr;
+    fst::ScaleDeterministicOnDemandFst *carpa_to_add_scale_ = nullptr;
+    // RNNLM rescoring
+    kaldi::rnnlm::RnnlmComputeStateInfo *rnnlm_info_ = nullptr;
+    kaldi::rnnlm::KaldiRnnlmDeterministicFst* rnnlm_to_add_ = nullptr;
+    fst::DeterministicOnDemandFst<fst::StdArc> *rnnlm_to_add_scale_ = nullptr;
+
 
     float sampling_frequency_;
     int32 frame_offset_;
@@ -45,7 +61,11 @@ private:
     int64 samples_round_start_;
     RecognizerState state_;
 
-
+    int max_alternatives_ = 0; // Disable alternatives by default
+    bool words_ = false;
+    bool partial_words_ = false;
+    bool nlsml_ = false;
+    string last_result_;
 
 };
 
