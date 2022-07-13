127b841cd7c8edac5ed30f2c11250e860648b3ad
kaicheng.wang <kwang1993@qq.com>
2022-07-13 21:24:05 +0800

    Removed deprecated audiorecord part.

--- app/src/main/java/com/example/kwang/kaldiandroid/MainActivity.java
+++ app/src/main/java/com/example/kwang/kaldiandroid/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ToggleButton;
 
-import com.example.kwang.kaldiandroid.audiorecord.RecordingService;
 import com.example.kwang.kaldiandroid.services.RecognitionListener;
 import com.example.kwang.kaldiandroid.services.SpeechService;
 import com.example.kwang.kaldiandroid.util.Model;
